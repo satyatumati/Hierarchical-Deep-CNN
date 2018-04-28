cf8521e1affac8b6f389c9661050933a4c1dafa5
TSatyaVasanthReddy <satyavasanthreddy@gmail.com>
2018-04-27 23:37:26 -0700

    class analysis

--- download_image.py
+++ download_image.py
@@ -2,7 +2,20 @@ import numpy as np
 import pandas as pd 
 import sys, requests, shutil, os
 from urllib import request, error
+import sys
 
+data=pd.read_csv('train.csv')
+data.head(5)
+def split(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
 data=pd.read_csv('train.csv')
 data.head(5)
 
@@ -16,12 +29,23 @@ links=data['url']
 ids = data['landmark_id']
 i=0
 
-for link,id_ in zip(links,ids):              #looping over links to get images
-    if os.path.exists('train/'+str(i)+'.jpg'):
+links=data['url']
+linkslist = split(links,20)
+ids = data['landmark_id']
+idslist = split(ids,20)
+i=0
+for j in range(0,20):
+    print(j)
+    links = linkslist[j]
+    ids = idslist[j]
+    for link,id_ in zip(links,ids):              #looping over links to get images
+        if os.path.exists('train/'+str(i)+'.jpg'):
+            i+=1
+            continue
+        fetch_image(link)
+        if(not os.path.isdir('images/train_images/'+str(id_))):
+        	os.mkdir('images/train_images/'+str(id_))
+        os.rename('images/train_images/image.jpg','images/train_images/'+str(id_)+'/'+ str(i)+'_'+str(id_)+ '.jpg')
         i+=1
-        continue
-    fetch_image(link)
-    os.rename('images/train_images/image.jpg','images/train_images/'+ str(i)+'_'+str(id_)+ '.jpg')
-    i+=1
-    #if(i==15):   #uncomment to test in your machine
-    #    break
+        #if(i==15):   #uncomment to test in your machine
+        #    break
